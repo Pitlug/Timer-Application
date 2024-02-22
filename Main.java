b29f596abb4e6577e17af5dc790c5b193cc6ab0b
Pitlug <94481283+Pitlug@users.noreply.github.com>
2024-02-22 14:13:42 -0500

    Timer button Method
    Partially created timer button. I will return later and start creating the GUI using javax.swing to be able to make the GUI full customization.

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.time.LocalDateTime; // Import the LocalDateTime class
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+
 public class Main {
     public static void main(String[] args) {
-
+    	System.out.println("The time taken in milliseconds is: "+timerbutton());
     }
+    static long timerbutton() {
+    	long start = System.currentTimeMillis();
+    	// when button is clicked the rest of the function will execute
+    	
+    	long end = System.currentTimeMillis() - start;
+    	return end;
+    	}
+    
 }
